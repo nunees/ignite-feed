050e093985e1c6a62eeb33694b086a541fa25986
nunees <felipe_silva_nunes@hotmail.com>
2022-07-05 19:29:16 -0300

    feat: add avatar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
 
 import styles from "./Sidebar.module.css";
 
@@ -10,7 +11,7 @@ export function Sidebar(){
             src="https://images.unsplash.com/photo-1522252234503-e356532cafd5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
             />
             <div className={styles.profile} >
-                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/28915121?v=4" />
+                <Avatar src="https://avatars.githubusercontent.com/u/28915121?v=4" />
                 <strong>Felipe Nunes</strong>
                 <span>Web Developer</span>
             </div>
