8d8420f5e3c661d1279c0c13e9b37b5af4c29c0e
nunees <felipe_silva_nunes@hotmail.com>
2022-07-05 18:44:54 -0300

    feat: add comment component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,33 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment(){
+    return(
+        <div className={styles.comment}>
+            <img src="https://avatars.githubusercontent.com/u/28915121?v=4" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Felipe Nunes</strong>
+                            <time title="11 de maio as 11:13" dateTime="2022-05-11 08:13:00">Cerca de 1 hora atras</time>
+                        </div>
+
+                        <button title="Deletar comentario">
+                            <Trash size={20}/>
+                        </button>
+                    </header>
+
+                    <p>Muito bom Devon!!!</p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    );
+}
